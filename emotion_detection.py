554555b258ebeb889ea4f77b99398c3ec1a3c85a
Annapurnaporia <annapurnaporia4@gmail.com>
2026-04-07 17:17:28 +0530

    new changes

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,29 @@
-URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-Input json: { "raw_document": { "text": text_to_analyse } }
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """
+    This function sends text to Watson NLP Emotion API
+    and returns the 'text' part of the response.
+    """
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    # Send POST request
+    response = requests.post(url, json=input_json, headers=headers)
+
+    # Convert response to JSON
+    response_json = response.json()
+
+    # Return only the 'text' part of response
+    return response_json["document_tone"]["tones"]
